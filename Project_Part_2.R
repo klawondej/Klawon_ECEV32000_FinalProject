ee5b1efc9ebbc50088b22f87942f58dd1f68803e
klawondej <36243727+klawondej@users.noreply.github.com>
2018-03-21 21:30:25 -0500

    Delete Project_Part_2.R

--- Project_Part_2.R
+++
@@ -1,9 +0,0 @@
-#run this code after compiling all CDR3 and TCRs from mice of interest using the
-#python script: 'Project Part 1'
-library(tidyverse)
-library(ggplot2)
-> MyData <- read.csv(file="c:/Users/Dave Klawon/Desktop/Biocomputing final project/Top_TCRs_Compiled_All_Mice.csv", header=TRUE, sep=",")
-> View(MyData)
-> p6 <- ggplot(MyData, aes(x = Percent.of.Rep, y = CDR3, size = Num.of.Mice.Appearing)) +
-  +     geom_point()
-> p6
